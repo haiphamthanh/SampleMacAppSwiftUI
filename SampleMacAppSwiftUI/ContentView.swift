0b090494af17fcdd5e9d92e094b171d37042468e
HaiPham <haipham@HaiPhams-MacBook-Pro.local>
2022-07-24 22:56:20 +0700

    [init]

---
+++ SampleMacAppSwiftUI/ContentView.swift
@@ -0,0 +1,105 @@
+//
+//  ContentView.swift
+//  SampleMacAppSwiftUI
+//
+//  Created by HaiPham on 24/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	@State private var text: String = """
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	"""
+	
+	var body: some View {
+		VStack {
+			TextEditor(text: $text)
+				.font(.title)
+				.frame(minWidth: 700, minHeight: 500)
+			
+			HStack {
+				// New text button.
+				Button(action: {
+					text = ""
+				}, label: {
+					HStack {
+						Image(systemName: "plus")
+						Text("New")
+					}
+					.frame(width: 80)
+				})
+				
+				Spacer()
+				
+				// Save button.
+				Button(action: {
+					let saveURL = showSavePanel()
+					writeText(to: saveURL)
+				}, label: {
+					HStack {
+						Image(systemName: "square.and.arrow.down")
+						Text("Save")
+					}
+					.frame(width: 80)
+				})
+				
+				// Open button.
+				Button(action: {
+					let openURL = showOpenPanel()
+					readText(from: openURL)
+				}, label: {
+					HStack {
+						Image(systemName: "square.and.arrow.up")
+						Text("Open")
+					}
+					.frame(width: 80)
+				})
+			}
+			.padding(20)
+		}
+	}
+	
+	
+	func showSavePanel() -> URL? {
+		let savePanel = NSSavePanel()
+		savePanel.allowedContentTypes = [.text]
+		savePanel.canCreateDirectories = true
+		savePanel.isExtensionHidden = false
+		savePanel.allowsOtherFileTypes = false
+		savePanel.title = "Save your text"
+		savePanel.message = "Choose a folder and a name to store your text."
+		savePanel.nameFieldLabel = "File name:"
+		
+		let response = savePanel.runModal()
+		return response == .OK ? savePanel.url : nil
+	}
+	
+	
+	func writeText(to url: URL?) {
+		guard let url = url else { return }
+		try? text.write(to: url, atomically: true, encoding: .utf8)
+	}
+	
+	
+	func showOpenPanel() -> URL? {
+		let openPanel = NSOpenPanel()
+		openPanel.allowsMultipleSelection = false
+		openPanel.canChooseDirectories = false
+		openPanel.canChooseFiles = true
+		let response = openPanel.runModal()
+		return response == .OK ? openPanel.url : nil
+	}
+	
+	func readText(from url: URL?) {
+		guard let url = url else { return }
+		guard let loadedText = try? String(contentsOf: url) else { return }
+		text = loadedText
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		ContentView()
+	}
+}
---
+++ SampleMacAppSwiftUI/SampleMacAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleMacAppSwiftUIApp.swift
+//  SampleMacAppSwiftUI
+//
+//  Created by HaiPham on 24/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SampleMacAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
